1950af6d48a26f9c048904281f2de04e55b2124f
dennissammy77 <dennissammy77@gmail.com>
2025-02-25 12:29:52 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = [];
+cats.push("Milo", "Otis", "Garfield");
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name)
+  return 
+};
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+  return 
+};
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop()
+  return 
+};
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name)
+  return 
+};
+
+function appendCat(name) {
+  const new_arr = [...cats,name]
+  return new_arr 
+};
+
+function prependCat(name) {
+  const new_arr = [name,...cats]
+  return new_arr 
+};
+
+function removeLastCat(name) {
+  const new_arr = [...cats]
+  new_arr.pop();
+  return new_arr; 
+};
+
+function removeFirstCat(name) {
+  const new_arr = [...cats]
+  new_arr.shift();
+  return new_arr; 
+};
